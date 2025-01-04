b8ddd07ca03a575af3742112f7041c2555890321
enockkim <enocksang@hotmail.com>
2025-01-04 19:35:48 +0300

    maybe please

--- prema.shuleone.web.client/src/app/routes/routes-routing.module.ts
+++ prema.shuleone.web.client/src/app/routes/routes-routing.module.ts
@@ -10,7 +10,7 @@ import { RegisterComponent } from './sessions/register/register.component';
 import { Error403Component } from './sessions/403.component';
 import { Error404Component } from './sessions/404.component';
 import { Error500Component } from './sessions/500.component';
-import { authGuard } from '@core';
+import { authGuard } from '../core/authentication/auth.guard';
 
 const routes: Routes = [
   {
